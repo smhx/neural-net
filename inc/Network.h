f437c5fd004ffd509252d96197ebd3790fa55c63
Steven Mai <stevenmai132@yahoo.ca>
2017-11-02 16:17:42 -0400

    Network class

--- inc/Network.h
+++ inc/Network.h
@@ -1,6 +1,16 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-void test();
+#include <vector>
+
+struct Data {};
+
+class Network {
+public:
+	Network(const std::vector<int>& sizes);
+	void SGD(const std::vector<Data>& data, int numEpochs, double trainingRate);
+private:
+
+};
 
 #endif
