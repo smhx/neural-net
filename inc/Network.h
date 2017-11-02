e5bcb023e4c3e32fa36dc5cdc767f970eac44fd0
Steven Mai <stevenmai132@yahoo.ca>
2017-11-02 17:19:53 -0400

    added a bit to Network

--- inc/Network.h
+++ inc/Network.h
@@ -2,15 +2,35 @@
 #define NETWORK_H
 
 #include <vector>
+#include <cstdlib> // random
 
 #include "Data.h"
 
+using namespace std;
+
 class Network {
 public:
-	Network(const std::vector<int>& sizes);
-	void SGD(const std::vector<Data>& data, int numEpochs, double trainingRate);
+	Network(const vector<int>& sizes);
+	void SGD(const vector<Data>& data, int numEpochs, double trainingRate);
 private:
-	
+
+	void updateBatch(vector<Data> batch);
+
+	double trainingRate;
+
+	// the size of layer i with layer 0 = input layer
+	vector<int> layerSizes;
+
+	// size of weights is number of layers - 1
+	// each element of weights ia a weight matrix from layer i to layer i+1
+	// so weights[i] is a layerSizes[i] x layerSizes[i+1] matrix
+	vector< vector< vector<double> > > weights; 
+
+	// bias of layer i
+	// skip i = 0 cuz input has no bias
+	vector< vector<double> > bias;
+
+
 };
 
 #endif
