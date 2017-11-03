3551797c43a1c95a61db021d2c7eecb8dd455b48
Steven Mai <stevenmai132@yahoo.ca>
2017-11-03 15:18:14 -0400

    stuff

--- inc/Data.h
+++ inc/Data.h
@@ -1,10 +1,16 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <vector>
+
 struct Data {
-	double in, out;
+	typedef std::vector<double> vdbl;
+
+	int in, out;
+	vdbl inlayer, outlayer;
+
 	Data(int _in, int _out);
-	Data(const Data& data);
+	Data(int _in, const vdbl& _outlayer);
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <cstdio>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
 #include "inc/Network.h"
+#include "inc/Data.h"
+
+using namespace std;
+
+const int MAX = 100;
+
+int f(int x) {
+	return x+1;
+}
 
 int main() {
-	// printf("%d\n", MAXN);
-	// vector<int> a;
-	//Howard's test comment
-	// Steven's test commend
+	srand(time(NULL));
+
+	vector<Data> data;
+	vector<int> layerSizes;
+
+	int nLayers, numEpochs, batchSize;
+	double trainingRate;
+
+	scanf("%d", &nLayers);
+	for (int i = 0; i < nLayers; ++i) {
+		int sz; scanf("%d", &sz);
+		layerSizes.push_back(sz);
+	}
+
+	for (int i = 0; i < layerSizes[0]; ++i) {
+		int x = rand() % MAX;
+		data.push_back(Data(x, f(x) ));
+	}
+
+	scanf("%d %d %lf", &numEpochs, &batchSize, &trainingRate);
+
+	Network AI(layerSizes);
+	AI.SGD(data, numEpochs, batchSize, trainingRate);
+	return 0;
 }
