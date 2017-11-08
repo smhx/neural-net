fbdf1b4887f1e833ff04d7228273a654c2304a24
Steven Mai <stevenmai132@yahoo.ca>
2017-11-07 21:24:42 -0500

    digit things

--- makefile
+++ makefile
@@ -7,6 +7,13 @@ main: build/main.o build/Network.o
 build/main.o: main.cpp inc/Network.h 
 	$(CXX) $(CXXFLAGS) -c -o build/main.o main.cpp 
 
+digit: build/digit.o build/tester.o build/Network.o
+	$(CXX) $(CXXFLAGS) -o bin/digit build/digit.o build/tester.o build/Network.o
+
+build/digit.o: test/digit/digit.cpp inc/Network.h test/tester.h
+	$(CXX) $(CXXFLAGS) -c -o build/digit.o test/digit/digit.cpp
+
+
 gender: build/gender.o build/tester.o build/Network.o
 	$(CXX) $(CXXFLAGS) -o bin/gender build/gender.o build/tester.o build/Network.o
 
--- test/digit/digit.cpp
+++ test/digit/digit.cpp
@@ -1,46 +1,88 @@
 #include "../tester.h"
-#include <fstream>
+#include <string>
+#include <cstdio>
 using namespace std;
 
-const int ndigits=10;
+const int ndigits=10, imgsize=28;
 
-int imgsize, ntraining, ntest;
-/*
-void read(Tester& tester) {
+int ntraining, ntest;
+
+// taken from https://compvisionlab.wordpress.com/
+
+inline int revint(int i) {
+	unsigned char ch1, ch2, ch3, ch4;
+    ch1=i&255;
+    ch2=(i>>8)&255;
+    ch3=(i>>16)&255;
+    ch4=(i>>24)&255;
+    return((int)ch1<<24)+((int)ch2<<16)+((int)ch3<<8)+ch4;
+}
+
+void readimg(string fname, trbatch& tr) {
+	ifstream file(fname, ios::binary);
+	int magic_number=0;
+    int number_of_images=0;
+    int n_rows=0;
+    int n_cols=0;
+    file.read((char*)&magic_number,sizeof(magic_number));
+    magic_number= revint(magic_number);
+    file.read((char*)&number_of_images,sizeof(number_of_images));
+    number_of_images= revint(number_of_images);
+    file.read((char*)&n_rows,sizeof(n_rows));
+    n_rows= revint(n_rows);
+    file.read((char*)&n_cols,sizeof(n_cols));
+    n_cols= revint(n_cols);
+
+    printf("n_rows = %d, n_cols = %d\n", n_rows, n_cols);
+
+    tr = trbatch(number_of_images);
+    
+    for(int i=0;i<number_of_images;++i) {
+    	tr[i].first = vdbl(n_rows * n_cols);
+        for(int r=0;r<n_rows;++r){
+            for(int c=0;c<n_cols;++c){
+                unsigned char temp=0;
+                file.read((char*)&temp,sizeof(temp));
+                tr[i].first[(n_rows*r)+c]= (double)temp;
+            }
+        }
+    }
+}
+
+void readlbl(string fname, trbatch& tr) {
 	
-	ifstream fin("test/digit/imgdata.txt");
+    ifstream file (fname, ios::binary);
 
-	fin >> imgsize >> ntraining >> ntest;
+    int magic_number = 0;
+    int number_of_images = 0;
+    file.read((char*) &magic_number, sizeof(magic_number));
+    magic_number = revint(magic_number);
+    file.read((char*) &number_of_images,sizeof(number_of_images));
+    number_of_images = revint(number_of_images);
 
-	tester.training = trbatch(ntraining);
+    for(int i = 0; i < number_of_images; ++i){
+        unsigned char temp = 0;
+        file.read((char*) &temp, sizeof(temp));
+        tr[i].second = vdbl(ndigits, 0.0);
+        tr[i].second[temp] = 1.0;
+    }
+	
+}
 
-	for (int i = 0; i < ntraining; ++i) {
-		for (int j = 0; j < imgsize * imgsize; ++j) {
-			double v; fin >> v;
-			tester.training[i].first.push_back(v);
-		}
-		int dig; fin >> dig;
-		tester.training[i].second = vdbl(ndigits, 0.0);
-		tester.training[i].second[dig] = 1.0;
-	}
 
-	tester.testing = trbatch(ntest);
+void read(Tester& t) {
+	readimg("test/digit/data/trainimg", t.training);
+	readimg("test/digit/data/testimg", t.testing);
 
-	for (int i = 0; i < ntest; ++i) {
-		for (int j = 0; j < imgsize * imgsize; ++j) {
-			double v; fin >> v;
-			tester.testing[i].first.push_back(v);
-		}
-		int dig; fin >> dig;
-		tester.testing[i].second = vdbl(10, 0.0);
-		tester.testing[i].second[dig] = 1.0;
-	}	
+	readlbl("test/digit/data/trainlbl", t.training);
+	readlbl("test/digit/data/testlbl", t.testing);
 }
 
 void train(){
 	Tester tester;
-	read(tester); // reads tester.training and tester.testing
-	tester.sizes = {imgsize, 100, ndigits};
+	read(tester);
+	cout << "Finished reading\n";
+	tester.sizes = {imgsize*imgsize, 100, ndigits};
 
 	tester.numEpochs = 200;
 	tester.batchSize = 20;
@@ -58,4 +100,4 @@ void train(){
 
 int main() {
 	train();
-}*/
+}
