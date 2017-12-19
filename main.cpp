a88917090c13af0a5587f9c88692a769eb530726
Plasma-Vortex <howardhalim@gmail.com>
2017-12-19 14:53:45 -0500

    My last update didn't get saved

--- main.cpp
+++ main.cpp
@@ -1,95 +1,165 @@
-#include <cstdio>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
+// #include <cstdio>
+// #include <vector>
+// #include <ctime>
+// #include <cstdlib>
 
-#include "inc/Network2.h"
-#include "inc/Layer.h"
-#include "inc/types.h"
-#include "inc/FullyConnectedLayer.h"
-#include "inc/ActivationFunction.h"
+// #include "inc/Network2.h"
+// #include "inc/Layer.h"
+// #include "inc/types.h"
+// #include "inc/FullyConnectedLayer.h"
+// #include "inc/ActivationFunction.h"
 
-#include <Eigen/Dense>
+// #include <Eigen/Dense>
 
-typedef long long ll;
+// typedef long long ll;
 
 using namespace std;
 
-Vec binary(ll i, int bits)
-{
-	Vec v(bits);
-	for (int j = 0; j < bits; ++j) {
-		if (i & (1LL << j))
-			v[j] = 1.0;
-		else
-			v[j] = -1.0;
-	}
-	return v;
-}
+// using namespace std;
 
-vdbl mod10(long long i)
-{
-	vdbl v(10, 0.0);
-	v[i % 10] = 1.0;
-	return v;
-}
+// Vec binary(ll i, int bits)
+// {
+// 	Vec v(bits);
+// 	for (int j = 0; j < bits; ++j) {
+// 		if (i & (1LL << j))
+// 			v[j] = 1.0;
+// 		else
+// 			v[j] = -1.0;
+// 	}
+// 	return v;
+// }
 
-pair<int,double> check(const Mat& tocheck, const Mat& correct) {
-	if (tocheck.rows() != correct.rows() || tocheck.cols() != correct.cols()) {
-		printf("ERROR in check: Vectors are different sizes\n");
-		return make_pair(0,0);
-	}
-//	cout << tocheck.col(0) << endl;
-//	cout << correct.col(0) << endl;
-//	cout << tocheck << endl;
-	int count = 0;
-	double cost = 0.0;
-	for (int col = 0; col < tocheck.cols(); col++)
-	{
-		bool works = true;
-		for (int i = 0; i < tocheck.rows(); ++i) {
-			double error = abs(tocheck(i, col) - correct(i, col));
-			if (error >= 1.0)
-				works = false;
-			cost += error*error;
-		}
-		if (works)
-			++count;
-	}
-	if (isnan(cost))
-	{
-//		cout << "Cost is nan!\nTest Batch:\n" << tocheck << "\nAnswers\n" << correct;
-	}
-	return make_pair(count, cost/tocheck.cols());
-}
+// vdbl mod10(long long i)
+// {
+// 	vdbl v(10, 0.0);
+// 	v[i % 10] = 1.0;
+// 	return v;
+// }
 
-int main() {
+// pair<int,double> check(const Mat& tocheck, const Mat& correct) {
+// 	if (tocheck.rows() != correct.rows() || tocheck.cols() != correct.cols()) {
+// 		printf("ERROR in check: Vectors are different sizes\n");
+// 		return make_pair(0,0);
+// 	}
+// //	cout << tocheck.col(0) << endl;
+// //	cout << correct.col(0) << endl;
+// //	cout << tocheck << endl;
+// 	int count = 0;
+// 	double cost = 0.0;
+// 	for (int col = 0; col < tocheck.cols(); col++)
+// 	{
+// 		bool works = true;
+// 		for (int i = 0; i < tocheck.rows(); ++i) {
+// 			double error = abs(tocheck(i, col) - correct(i, col));
+// 			if (error >= 1.0)
+// 				works = false;
+// 			cost += error*error;
+// 		}
+// 		if (works)
+// 			++count;
+// 	}
+// 	if (isnan(cost))
+// 	{
+// //		cout << "Cost is nan!\nTest Batch:\n" << tocheck << "\nAnswers\n" << correct;
+// 	}
+// 	return make_pair(count, cost/tocheck.cols());
+// }
 
-	srand(time(NULL));
+// int main() {
 
-	int bits = 11;	
+// 	srand(time(NULL));
 
-	typedef FullyConnectedLayer<SigmoidActivationFunction> SigLayer;
+// 	int bits = 11;	
+
+// 	typedef FullyConnectedLayer<SigmoidActivationFunction> SigLayer;
 	
-	vector<Layer*> layers;
-	layers.push_back(new SigLayer(2 * bits, 5 * bits));
-	layers.push_back(new SigLayer(5 * bits, 2 * bits));
+// 	vector<Layer*> layers;
+// 	layers.push_back(new SigLayer(2 * bits, 5 * bits));
+// 	layers.push_back(new SigLayer(5 * bits, bits + 1));
 
-	Network2 n(layers, check, 2 * bits, 2 * bits, 8, 0.1);
+// 	Network2 n(layers, check, 2 * bits, 2 * bits, 8, 0.001, 0.0);
 
-	trbatch training(100000), testing(1000);
+// 	trbatch training(100000), testing(1000);
 	
-	for (trdata& data : testing) {
-		ll i = rand() & ((1 << bits) - 1);
-		ll j = rand() & ((1 << bits) - 1);
-		data.first = binary((i << bits) + j, 2 * bits);
-		data.second = binary(i * j, 2 * bits);
-	}		
-	for (trdata& data : training) {
-		ll i = rand() & ((1 << bits) - 1);
-		ll j = rand() & ((1 << bits) - 1);
-		data.first = binary((i << bits) + j, 2 * bits);
-		data.second = binary(i * j, 2 * bits);
-	}
-	n.train(training, testing, 500);
-}
+// 	for (trdata& data : testing) {
+// 		ll i = rand() & ((1 << bits) - 1);
+// 		ll j = rand() & ((1 << bits) - 1);
+// 		data.first = binary((i << bits) + j, 2 * bits);
+// 		data.second = binary(i * j, 2 * bits);
+// 	}
+// 	for (int i = 0; i < (1 << (2*bits)); i++) {
+// 		testing[i].first = binary(i, 2 * bits);
+// 		testing[i].second = binary((i >> bits) + (i & ((1 << bits) - 1)), bits + 1);
+// 	}
+// 		data.second = binary(i + j, bits + 1);
+// 	}
+	
+// 	// for (int i = 0; i < (1 << (2*bits)); i++) {
+// 	// 	testing[i].first = binary(i, 2 * bits);
+// 	// 	testing[i].second = binary((i >> bits) + (i & ((1 << bits) - 1)), bits + 1);
+// 	// }
+
+// 	for (trdata& data : training) {
+// 		ll i = rand() & ((1 << bits) - 1);
+// 		ll j = rand() & ((1 << bits) - 1);
+// 		data.first = binary((i << bits) + j, 2 * bits);
+// 		data.second = binary(i * j, 2 * bits);
+// 	}
+
+// //	cout << testing[5].first << endl;
+// //	cout << testing[5].second << endl;
+// 	n.train(training, testing, 500);
+	
+// }
+// /*
+// vdbl binary(long long i, int bits)
+// {
+// 	vdbl v(bits, 0.0);
+// 	for (int j = 0; j < bits; ++j)
+// 	{
+// 		if (i&(1LL << j))
+// 			v[j] = 1.0;
+// 	}
+// 	return v;
+// }
+
+// bool check(const vdbl& tocheck, const vdbl& correct)
+// {
+// 	if (tocheck.size() != correct.size())
+// 	{
+// 		printf("ERROR different size\n");
+// 		return false;
+// 	}
+// 	bool works = true;
+// 	for (int i = 0; i < tocheck.size() && works; ++i)
+// 	{
+// 		if (abs(tocheck[i] - correct[i]) >= 0.5) works = false;
+// 	}
+// 	return works;
+// }
+
+// int main()
+// {
+// 	srand(time(NULL));
+// //	ifstream fin("tests/test.txt");
+// 	int bits = 8;
+// 	vector<int> sizes({ 2*bits, 5*bits, bits+1 });
+// 	Network n(sizes, check, 10, 1, 1, 0.2, 0, 0);
+// //	Network n(fin, check);
+// 	vector<trdata> training(20000), testing(1000);
+
+// 	for (trdata& data : testing) {
+// 		int i = rand() & ((1 << bits) - 1);
+// 		int j = rand() & ((1 << bits) - 1);
+// 		data.first = binary((i << bits) + j, 2 * bits);
+// 		data.second = binary(i + j, bits + 1);
+// 	}
+// 	for (trdata& data : training) {
+// 		int i = rand() & ((1 << bits) - 1);
+// 		int j = rand() & ((1 << bits) - 1);
+// 		data.first = binary((i << bits) + j, 2 * bits);
+// 		data.second = binary(i + j, bits + 1);
+// 	}
+// 	n.SGD(training, testing, 100, "tests/oldNetwork.txt");
+// }
+// */
